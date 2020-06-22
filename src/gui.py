901ff815a3cba00d47d93a6be34b580b1bd45ad5
bami <bamibehrami9@gmail.com>
2020-06-22 13:47:36 +0200

    Init GUI stuff but its actually super shit

--- src/gui.py
+++ src/gui.py
@@ -1,1 +1,13 @@
-"""TODO: Show data in nice GUI"""
+import tkinter as tk
+
+
+root = tk.Tk()
+root.title('League Account Manager')
+root.geometry('500x500')
+
+button = tk.Button(text='Add Account', command=lambda: print('Pressed'))
+button.pack()
+
+listbox = tk.Listbox(root)
+listbox.pack()
+root.mainloop()
